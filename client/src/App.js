c4d55d3039e570fb3699f691ddccb4a66713b868
Adith <adithdinesh97@gmail.com>
2021-04-04 18:59:52 +0530

    [UPDATE] Basic React structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,24 @@
+import AdminPanel from './components/adminpanel/AdminPanel';
+import Form from './components/form/Form';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
 function App() {
-  return <div className='App'>Work in progress</div>;
+  return (
+    <Router>
+      <div className='App'>
+        <Switch>
+          <Route exact path='/admin' component={AdminPanel} />
+          <Route exact path='/' component={Form} />
+          <Route render={() => <Redirect to='/' />} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
